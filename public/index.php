c35f0e43bc5ef0ecf44f6c8cf818dacffa6eb1a7
Fronsky <123108076+fronskyofficial@users.noreply.github.com>
2023-02-20 17:54:30 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Define constants
-define('PUBLIC_PATH', 'http://localhost/public/');
+define('PUBLIC_PATH', 'http://localhost/snabbstar/public/');
 define('BASE_PATH', '../secure/');
 define('VIEW_PATH', BASE_PATH . 'views/');
 define('INCLUDE_PATH', BASE_PATH . 'includes/');
@@ -17,24 +17,23 @@ require BASE_PATH . 'app/enums/ErrorDefinitions.php';
  * @return void
  */
 function loadFile($path) {
-    // Output the default styling for all views
     echo '<link rel="shortcut icon" href="'.PUBLIC_PATH.'favicon.ico" type="image/x-icon">';
     echo '<link rel="icon" href="'.PUBLIC_PATH.'images/icon.png">';
     echo '<link rel="apple-touch-icon" href="'.PUBLIC_PATH.'images/icon.png">';
     echo '<link rel="preconnect" href="https://fonts.googleapis.com">';
     echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>';
     echo '<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap">';
+    echo '<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons">';
+    echo '<link rel="stylesheet" type="text/css" href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css">';
     echo '<link rel="stylesheet" type="text/css" href="'.PUBLIC_PATH.'css/core/bootstrap.min.css">';
     echo '<script defer src="'.PUBLIC_PATH.'js/core/jquery.min.js"></script>';
     echo '<script defer src="'.PUBLIC_PATH.'js/core/bootstrap.min.js"></script>';
     echo '<link rel="stylesheet" type="text/css" href="'.PUBLIC_PATH.'css/site.css">';
     echo '<script defer src="'.PUBLIC_PATH.'js/site.js"></script>';
 
-    // Load the specified file
     if (file_exists($path)) {
         require $path;
     } else {
-        // Handle file not found error
         http_response_code(404);
         $errorFile = ErrorDefinitions::err404->value;
         if (!file_exists($errorFile)) {
@@ -44,7 +43,6 @@ function loadFile($path) {
                 $errorFile = null;
             }
         }
-        // Load the error file
         if ($errorFile !== null) {
             require $errorFile;
         } else {
@@ -56,15 +54,36 @@ function loadFile($path) {
 // Route based on the URL
 $url = explode('/', trim($_SERVER['PATH_INFO'] ?? '', '/'));
 $viewFile = DEFAULT_VIEW;
+
 if (!empty($url[0])) {
     $fileName = pathinfo($url[0], PATHINFO_FILENAME);
     $fileExtension = pathinfo($url[0], PATHINFO_EXTENSION);
-    // Determine the file path based on the file extension
+
     if (in_array($fileExtension, ['inc', 'include', 'includes'])) {
-        $viewFile = INCLUDE_PATH . $fileName . '.inc.php';
+        if (isset($url[1])) {
+            $fileName1 = pathinfo($url[0], PATHINFO_FILENAME);
+            $fileName2 = pathinfo($url[1], PATHINFO_FILENAME);
+            $viewFile = INCLUDE_PATH . $fileName1 . '/' . $fileName2 . '.inc.php';
+        } else {
+            $viewFile = INCLUDE_PATH . $fileName . '.inc.php';
+
+            if (!file_exists($viewFile)) {
+                $viewFile = VIEW_PATH . $fileName . '/index.inc.php';
+            }
+        }
     } else {
-        $viewFile = VIEW_PATH . $fileName . '.php';
+        if (isset($url[1])) {
+            $fileName1 = pathinfo($url[0], PATHINFO_FILENAME);
+            $fileName2 = pathinfo($url[1], PATHINFO_FILENAME);
+            $viewFile = VIEW_PATH . $fileName1 . '/' . $fileName2 . '.php';
+        } else {
+            $viewFile = VIEW_PATH . $fileName . '.php';
+            if (!file_exists($viewFile)) {
+                $viewFile = VIEW_PATH . $fileName . '/index.php';
+            }
+        }
     }
 }
+
 // Load the file
 loadFile($viewFile);
